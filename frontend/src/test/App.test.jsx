6db270d590050509ec9c5c76da367351ffd9d405
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2025-11-02 23:00:09 -0500

    Update App.test.jsx

--- frontend/src/test/App.test.jsx
+++ frontend/src/test/App.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from '../../App';
+import App from '../../App.jsx';
 
 describe('App component', () => {
   test('renders app root element', () => {
